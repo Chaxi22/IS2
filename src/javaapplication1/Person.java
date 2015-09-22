8e609a8eca53844e5154f00dc7cbd0ed0c2d60f2
usuario <usuario@10.22.143.14>
2015-09-22 09:58:27 +0100

    segunda version

--- src/javaapplication1/Person.java
+++ src/javaapplication1/Person.java
@@ -11,10 +11,11 @@ import java.util.Date;
  * @author usuario
  */
 public class Person {
-    
+
     private final String name;
     private final String surname;
     private final Date birthday;
+    private static final long MILLISECOND_PER_YEAR = (long) (1000 * 60 * 60 * 24 * 365.25);
 
     public Person(String name, String surname, Date birthday) {
         this.name = name;
@@ -33,13 +34,19 @@ public class Person {
     public Date getBirthday() {
         return birthday;
     }
-    
-    public String getFullName(){
-        return name+" "+surname;
+
+    public String getFullName() {
+        return name + " " + surname;
     }
-    
-    public int getAge(){
+
+    public int getAge() {
         Date today = new Date();
-        return (int) ((today.getTime()-birthday.getTime())/(1000*60*60*24*365.25));
+        return (int) miMetodo(today.getTime() - birthday.getTime());
+    }
+
+    private long miMetodo(long millis) {
+
+        return millis / MILLISECOND_PER_YEAR;
+
     }
 }
